8b950a96bdbdd5f0e985fc712bb96aa902fef663
KL <1667586782@qq.com>
2018-09-13 20:51:52 +0800

    sss
    Signed-off-by: KL <1667586782@qq.com>

---
+++ src/wc/FileCount.java
@@ -0,0 +1,12 @@
+package wc;
+import java.util.Scanner;
+import java.io.*;
+
+public class FileCount {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+	}
+
+}
